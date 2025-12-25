0d2951bdee3fd157a01ef4ec271ce39bd8dee7eb
nexussfdr <usikk272@gmail.com>
2025-12-25 01:21:22 -0300

    Create supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,5 @@
+// js/supabaseClient.js
+const SUPABASE_URL = "https://qdygfraboifbjjfyocyr.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_spDRa4LBpDGqdXpFQMvoDQ_F-_LNz6S";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
